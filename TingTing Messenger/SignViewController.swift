86d1be5081d1f4578066fc97f4016bd5c97a839e
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-04-16 10:06:57 +0530

    Login and Bassic Lottie Setup Done

---
+++ TingTing Messenger/SignViewController.swift
@@ -0,0 +1,82 @@
+//
+//  SignViewController.swift
+//  TingTing Messenger
+//
+//  Created by Henit Work on 16/04/21.
+//
+
+import UIKit
+import Lottie
+import Canvas
+
+class SignViewController: UIViewController {
+
+   
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextFeild: UITextField!
+    @IBOutlet weak var emailTextFeild: UITextField!
+
+    @IBOutlet weak var friendsAnimation: AnimationView!
+    @IBOutlet weak var backgroundSignUp: AnimationView!
+    @IBOutlet weak var bunnyHoping: AnimationView!
+    @IBOutlet weak var doggyAnimation: AnimationView!
+    @IBOutlet weak var mainLogView: CSAnimationView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainLogView.type = "zoomOut"
+        mainLogView.delay = 5
+        mainLogView.duration = 1
+        friendsAnimation.isHidden = true
+        bunnyHoping.loopMode = .playOnce
+        bunnyHoping.contentMode = .scaleAspectFit
+        bunnyHoping.play()
+       
+        emailTextFeild.layer.cornerRadius = 25
+        passwordTextFeild.layer.cornerRadius = 25
+        loginButton.layer.cornerRadius = 15
+        
+        
+        
+        DispatchQueue.main.async {
+            self.mainLogView.startCanvasAnimation()
+            
+        }
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            
+            self.backgroundSignUp.loopMode = .playOnce
+            self.backgroundSignUp.contentMode = .scaleAspectFill
+            self.backgroundSignUp.play()
+            self.friendsAnimation.isHidden = false
+            
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.doggyAnimation.loopMode = .loop
+            self.doggyAnimation.contentMode = .scaleAspectFill
+            self.doggyAnimation.play()
+            
+            self.friendsAnimation.layer.cornerRadius = 60
+            
+            self.friendsAnimation.loopMode = .playOnce
+            self.friendsAnimation.contentMode = .scaleAspectFill
+            self.friendsAnimation.play()
+        }
+        
+        
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TingTing Messenger/ViewController.swift
+++ TingTing Messenger/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pigeonView: AnimationView!
+    @IBOutlet weak var app_logo: AnimationView!
+    @IBOutlet weak var heart: AnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        app_logo.loopMode = .loop
+        app_logo.contentMode = .scaleAspectFill
+        app_logo.play()
+        
+        heart.loopMode = .loop
+        heart.contentMode = .scaleAspectFill
+        heart.play()
+        
+        pigeonView.loopMode = .loop
+        pigeonView.contentMode = .scaleAspectFill
+        pigeonView.play()
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.performSegue(withIdentifier: "toSign", sender: self)
+        }
     }
 
 
