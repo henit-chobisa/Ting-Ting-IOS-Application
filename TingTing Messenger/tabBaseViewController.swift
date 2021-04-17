b136d80b5fc49ebc685f3560beca7b73a9803704
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-04-18 04:08:34 +0530

    Home page Basic artributes setup

---
+++ TingTing Messenger/tabBaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  tabBaseViewController.swift
+//  TingTing Messenger
+//
+//  Created by Henit Work on 18/04/21.
+//
+
+import UIKit
+
+class tabBaseViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
